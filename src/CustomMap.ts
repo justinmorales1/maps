77635b2247057d59f177a1cf7225ee105355f23b
justinmorales1 <jm.em0210@gmail.com>
2021-07-04 11:27:32 -0500

    Updating CustomMap marker

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,7 +1,9 @@
-import { User } from './User';
-import { Company } from './Company';
-
-
+interface Mappable {
+  location: {
+    lat: number,
+    lng: number
+  }
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -16,24 +18,14 @@ export class CustomMap {
     });
   }
 
-addUserMarker(user: User) : void {
-  new google.maps.Marker({
-    map: this.googleMap,
-    position: {
-      lat: user.location.lat,
-      lng: user.location.lng
-    }
-  })
-}
-
-addCompanyMarker(company: Company) : void {
-  new google.maps.Marker({
-    map: this.googleMap,
-    position: {
-      lat: company.location.lat,
-      lng: company.location.lng
-    }
-  })
-}
+  addMarker(mappable: Mappable) : void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    })
+  }
 
 }
